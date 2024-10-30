ebf79c82c3f55e776c2fa4669f1e1c4b1481a197
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-10-29 22:44:05 -0600

    chore: init pulumi stuff

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,12 +1,18 @@
 import * as digitalocean from "@pulumi/digitalocean";
 import * as pulumi from "@pulumi/pulumi";
+import * as github from "@pulumi/github";
 import * as dotenv from "dotenv";
 dotenv.config();
-
 const config = new pulumi.Config();
 
-const web = new digitalocean.Droplet("web", {
-  name: "web",
-  image: "ubuntu-18-04-x64",
-  size: "s-1vcpu-1gb",
-});
+const setup = async () => {
+  const repoName = config.require("repository");
+  /**========================================================================
+   *                  Referencias recuros pre-existentes
+   *========================================================================**/
+  const repository = await github.getRepository({
+    fullName: repoName,
+  });
+};
+
+setup();
