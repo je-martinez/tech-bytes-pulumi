8fee842d0e8a9db869f135ead4b6b95262cf9efd
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-10-30 22:01:14 -0600

    chore: static website pulumi

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,16 +3,59 @@ import * as pulumi from "@pulumi/pulumi";
 import * as github from "@pulumi/github";
 import * as dotenv from "dotenv";
 dotenv.config();
+
 const config = new pulumi.Config();
 
 const setup = async () => {
+  const provider = new digitalocean.Provider("do-provider", {
+    token: process.env.DIGITALOCEAN_TOKEN,
+  });
+
   const repoName = config.require("repository");
+  const envName = config.require("environment");
+  const projectName = config.require("projectName");
+
   /**========================================================================
-   *                  Referencias recuros pre-existentes
+   *                  Ref to pre-existing resources
    *========================================================================**/
   const repository = await github.getRepository({
     fullName: repoName,
   });
+
+  const project = await digitalocean.getProject(
+    {
+      name: projectName,
+    },
+    { provider }
+  );
+
+  const branch = config.require("repository-branch");
+
+  const webApp = new digitalocean.App(
+    `tech-bytes-web-app-${envName}`,
+    {
+      projectId: project.id,
+      spec: {
+        name: `tech-bytes-web-app-${envName}`,
+        region: "nyc3",
+        staticSites: [
+          {
+            name: `tech-bytes-web-app-${envName}`,
+            //Don't forget to give access to the repository to your Digital Ocean Account
+            github: {
+              repo: repository.fullName,
+              branch,
+              deployOnPush: true,
+            },
+            sourceDir: "/web",
+            buildCommand: "npm install && npm run build",
+            outputDir: "/dist",
+          },
+        ],
+      },
+    },
+    { provider }
+  );
 };
 
 setup();
