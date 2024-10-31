1213eac3b50c3cd526a9c099db504dbbfdce52f7
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-10-30 22:42:44 -0600

    fix: port api

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app: Application = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
